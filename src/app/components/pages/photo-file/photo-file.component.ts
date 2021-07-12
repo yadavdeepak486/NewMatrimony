b23a840a08b1ac6982a8f66352b99804b92008f5
rohit0035 <rohitdevelopersveltosest@gmail.com>
2021-07-12 13:18:46 +0530

    new changes

---
+++ src/app/components/pages/photo-file/photo-file.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-file',
+  templateUrl: './photo-file.component.html',
+  styleUrls: ['./photo-file.component.scss']
+})
+export class PhotoFileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
