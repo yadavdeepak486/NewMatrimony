38834b97ccbadfcc5a78308181f7e6d3ba1ae601
developerdeveloper01 <deepakdevelopersveltosest01@gmail.com>
2021-07-21 11:05:50 +0530

    title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationCancel, NavigationEnd } from '@angular/router';
-import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import {
+  Location,
+  LocationStrategy,
+  PathLocationStrategy,
+} from '@angular/common';
 import { filter } from 'rxjs/operators';
 declare let $: any;
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    providers: [
-        Location, {
-            provide: LocationStrategy,
-            useClass: PathLocationStrategy
-        }
-    ]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  providers: [
+    Location,
+    {
+      provide: LocationStrategy,
+      useClass: PathLocationStrategy,
+    },
+  ],
 })
 export class AppComponent {
-    location: any;
-    routerSubscription: any;
+  location: any;
+  routerSubscription: any;
+  title: 'Matrimony';
 
-    constructor(private router: Router) {
-    }
+  constructor(private router: Router) {}
 
-    ngOnInit(){
-        this.recallJsFuntions();
-    }
+  ngOnInit() {
+    this.recallJsFuntions();
+  }
 
-    recallJsFuntions() {
-        this.routerSubscription = this.router.events
-        .pipe(filter(event => event instanceof NavigationEnd || event instanceof NavigationCancel))
-        .subscribe(event => {
-            $.getScript('../assets/js/main.js');
-            this.location = this.router.url;
-            if (!(event instanceof NavigationEnd)) {
-                return;
-            }
-            window.scrollTo(0, 0);
-        });
-    }
+  recallJsFuntions() {
+    this.routerSubscription = this.router.events
+      .pipe(
+        filter(
+          (event) =>
+            event instanceof NavigationEnd || event instanceof NavigationCancel
+        )
+      )
+      .subscribe((event) => {
+        $.getScript('../assets/js/main.js');
+        this.location = this.router.url;
+        if (!(event instanceof NavigationEnd)) {
+          return;
+        }
+        window.scrollTo(0, 0);
+      });
+  }
 }
