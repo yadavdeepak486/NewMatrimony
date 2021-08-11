a93f8b94cd774f63e0bb6031a32d041de6815a3a
rohit0035 <rohitdevelopersveltosest@gmail.com>
2021-08-11 16:57:58 +0530

    premium plan

--- src/app/components/pages/premuim-plan/premuim-plan.component.ts
+++ src/app/components/pages/premuim-plan/premuim-plan.component.ts
@@ -11,142 +11,230 @@ export class PremuimPlanComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  monthlyPricing = [
-    {
-        singlePricingBox: [
-            {
-                title: 'BRONZE Membership Plan',
-                features: [
-                    {
-                        icon: 'bx bx-check',
-                        text: '5 Listing'
-                    },
-                    {
-                        icon: 'bx bx-check',
-                        text: 'Non-Featured'
-                    },
-                    {
-                        icon: 'bx bx-check',
-                        text: '90 Days Availability'
-                    },
-                    {
-                        icon: 'bx bx-check',
-                        text: 'One Time Fee'
-                    },
-                    {
-                        icon: 'bx bx-check',
-                        text: 'Multi Team Tasking'
-                    },
-                    {
-                        icon: 'bx bx-check',
-                        text: 'Basic Registration & Ticketing'
-                    },
-                    {
-                        icon: 'bx bx-check',
-                        text: 'Basic Features'
-                    },
-                    {
-                        icon: 'bx bx-x action-close',
-                        text: 'Online Booking'
-                    },
-                    {
-                        icon: 'bx bx-x action-close',
-                        text: 'Limited Support'
-                    }
-                ],
-                price: '$9.99',
-                buttonText: 'Select Plan',
-                buttonLink: '#'
-            },
-            {
-                title: 'SILVER Memberships Plan',
-                features: [
-                    {
-                        icon: 'bx bx-check',
-                        text: '5 Listing'
-                    },
-                    {
-                        icon: 'bx bx-check',
-                        text: 'Non-Featured'
-                    },
-                    {
-                        icon: 'bx bx-check',
-                        text: '90 Days Availability'
-                    },
-                    {
-                        icon: 'bx bx-check',
-                        text: 'One Time Fee'
-                    },
-                    {
-                        icon: 'bx bx-check',
-                        text: 'Multi Team Tasking'
-                    },
-                    {
-                        icon: 'bx bx-check',
-                        text: 'Basic Registration & Ticketing'
-                    },
-                    {
-                        icon: 'bx bx-check',
-                        text: 'Basic Features'
-                    },
-                    {
-                        icon: 'bx bx-check',
-                        text: 'Online Booking'
-                    },
-                    {
-                        icon: 'bx bx-x action-close',
-                        text: 'Limited Support'
-                    }
-                ],
-                price: '$19.99',
-                buttonText: 'Select Plan',
-                buttonLink: '#'
-            },
-            {
-                title: 'GOLDEN Membership Plan',
-                features: [
-                    {
-                        icon: 'bx bx-check',
-                        text: '5 Listing'
-                    },
-                    {
-                        icon: 'bx bx-check',
-                        text: 'Non-Featured'
-                    },
-                    {
-                        icon: 'bx bx-check',
-                        text: '90 Days Availability'
-                    },
-                    {
-                        icon: 'bx bx-check',
-                        text: 'One Time Fee'
-                    },
-                    {
-                        icon: 'bx bx-check',
-                        text: 'Multi Team Tasking'
-                    },
-                    {
-                        icon: 'bx bx-check',
-                        text: 'Basic Registration & Ticketing'
-                    },
-                    {
-                        icon: 'bx bx-check',
-                        text: 'Basic Features'
-                    },
-                    {
-                        icon: 'bx bx-check',
-                        text: 'Online Booking'
-                    },
-                    {
-                        icon: 'bx bx-check',
-                        text: 'Limited Support'
-                    }
-                ],
-                price: '$29.99',
-                buttonText: 'Select Plan',
-                buttonLink: '#'
-            }
-        ]
-    }
-]
+//   monthlyPricing = [
+//     {
+//         singlePricingBox: [
+//             {
+//                 title: 'Gold ',
+//                 features: [
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: '5 Listing'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'Non-Featured'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: '90 Days Availability'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'One Time Fee'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'Multi Team Tasking'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'Basic Registration & Ticketing'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'Basic Features'
+//                     },
+//                     {
+//                         icon: 'bx bx-x action-close',
+//                         text: 'Online Booking'
+//                     },
+//                     {
+//                         icon: 'bx bx-x action-close',
+//                         text: 'Limited Support'
+//                     }
+//                 ],
+//                 price: '$9.99',
+//                 buttonText: 'Select Plan',
+//                 buttonLink: '#'
+//             },
+//             {
+//                 title: 'SILVER Memberships Plan',
+//                 features: [
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: '5 Listing'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'Non-Featured'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: '90 Days Availability'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'One Time Fee'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'Multi Team Tasking'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'Basic Registration & Ticketing'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'Basic Features'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'Online Booking'
+//                     },
+//                     {
+//                         icon: 'bx bx-x action-close',
+//                         text: 'Limited Support'
+//                     }
+//                 ],
+//                 price: '$19.99',
+//                 buttonText: 'Select Plan',
+//                 buttonLink: '#'
+//             },
+//             {
+//                 title: 'GOLDEN Membership Plan',
+//                 features: [
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: '5 Listing'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'Non-Featured'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: '90 Days Availability'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'One Time Fee'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'Multi Team Tasking'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'Basic Registration & Ticketing'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'Basic Features'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'Online Booking'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'Limited Support'
+//                     }
+//                 ],
+//                 price: '$29.99',
+//                 buttonText: 'Select Plan',
+//                 buttonLink: '#'
+//             },
+//             {
+//                 title: 'GOLDEN Membership Plan',
+//                 features: [
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: '5 Listing'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'Non-Featured'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: '90 Days Availability'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'One Time Fee'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'Multi Team Tasking'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'Basic Registration & Ticketing'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'Basic Features'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'Online Booking'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'Limited Support'
+//                     }
+//                 ],
+//                 price: '$29.99',
+//                 buttonText: 'Select Plan',
+//                 buttonLink: '#'
+//             },
+//             {
+//                 title: 'GOLDEN Membership Plan',
+//                 features: [
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: '5 Listing'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'Non-Featured'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: '90 Days Availability'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'One Time Fee'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'Multi Team Tasking'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'Basic Registration & Ticketing'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'Basic Features'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'Online Booking'
+//                     },
+//                     {
+//                         icon: 'bx bx-check',
+//                         text: 'Limited Support'
+//                     }
+//                 ],
+//                 price: '$29.99',
+//                 buttonText: 'Select Plan',
+//                 buttonLink: '#'
+//             }
+//         ]
+//     }
+// ]
 }
