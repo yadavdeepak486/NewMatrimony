71a523e3a2b2c049a8ca4cbaaf0f237a2165a3b6
rohit0035 <rohitdevelopersveltosest@gmail.com>
2021-08-14 10:37:50 +0530

    user chat

--- src/app/components/pages/my-profile/user-profile-one.component.ts
+++ src/app/components/pages/my-profile/user-profile-one.component.ts
@@ -20,9 +20,17 @@ export class UserProfileOneComponent implements OnInit {
   togglecheckpartner: boolean= true;
   togglecheckphysical: boolean= true;
   togglecheckproperties: boolean= true;
+  togglecheckhoroscope: boolean= true;
   dumPhoto = 'assets/img/me.jpg';
   Photodummen = 'assets/img/me.jpg';
   Photodumwomen = 'assets/img/wo.jpg';
+
+  educationslt = new FormControl();
+  educationsltList: string[] = ['10th', '12th', 'Polytechnic Diploma', 'Graduation', 'Post Graduation', ];
+  religionslt = new FormControl();
+  religionsltList: string[] = ['Hindu', 'Christian', 'Jain', ];
+  castslt = new FormControl();
+  castsltList: string[] = ['Jain', 'Christian', 'Balmiki','Bhanjra', ];
 
   editbasicinformation = new FormGroup({
     gender: new FormControl(''),
@@ -142,6 +150,15 @@ export class UserProfileOneComponent implements OnInit {
     }
     else{
       this.togglecheckproperties= false
+    }
+  }
+  horoscope(){
+    console.log("button clicked")
+    if (this.togglecheckhoroscope == false){
+      this.togglecheckhoroscope= true
+    }
+    else{
+      this.togglecheckhoroscope= false
     }
   }
 }
