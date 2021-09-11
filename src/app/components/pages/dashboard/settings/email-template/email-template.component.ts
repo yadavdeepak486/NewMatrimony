5dbb44fb131278b49f9616d4dcf03f169e86d3eb
rohit0035 <rohitdevelopersveltosest@gmail.com>
2021-09-11 18:34:59 +0530

    email-template change

--- src/app/components/pages/dashboard/settings/email-template/email-template.component.ts
+++ src/app/components/pages/dashboard/settings/email-template/email-template.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EditorChangeContent, EditorChangeSelection } from 'ngx-quill';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-email-template',
@@ -9,9 +10,52 @@ import { EditorChangeContent, EditorChangeSelection } from 'ngx-quill';
 export class EmailTemplateComponent implements OnInit {
   content: any = '';
   data: any;
+  toggleregisedit:boolean= true;
+  toggleactivateedit:boolean= true;
+  togglepaysuccess:boolean=true;
+  togglerequest:boolean= true;
+  togglerejectinterst: boolean= true;
+  togglenewmatch: boolean= true;
+  togglepassword: boolean= true;
 
   blured = false;
   focused = false;
+
+  paswordtemplateform = new FormGroup({
+    subject: new FormControl(''),
+    html: new FormControl(''),
+  });
+
+  registertemplateform = new FormGroup({
+    subject: new FormControl(''),
+    html: new FormControl(''),
+  });
+
+  memberactivateform = new FormGroup({
+    subject: new FormControl(''),
+    html: new FormControl(''),
+  });
+  
+  paymentsuccessform = new FormGroup({
+    subject: new FormControl(''),
+    html: new FormControl(''),
+  });
+
+  interestrequestform= new FormGroup ({
+    subject: new FormControl(''),
+    html: new FormControl(''),
+  });
+
+  interestrejectform = new FormGroup({
+    subject: new FormControl(''),
+    html: new FormControl(''),
+  });
+
+  newmatchform= new FormGroup ({
+    subject: new FormControl(''),
+    html: new FormControl(''),
+  });
+  
   constructor() { }
   ngOnInit(): void {
     
@@ -52,4 +96,103 @@ blur($event) {
 getValue(event: Event): string {
   return (event.target as HTMLInputElement).value;
 }
+
+registapprved(){
+  console.log('button clicked');
+    if(this.toggleregisedit == false){
+      this.toggleregisedit = true;
+    } else{
+      this.toggleregisedit = false;
+    }
+  }
+
+memberactivation(){
+    console.log('button clicked');
+    if(this.toggleactivateedit == false) {
+      this.toggleactivateedit= true;
+      } else {
+       this.toggleactivateedit= false;
+      }
+    }
+
+paymentsuccess(){
+  console.log('button clicked');
+  if(this.togglepaysuccess== false){
+    this.togglepaysuccess= true;
+  } else{
+    this.togglepaysuccess= false;
+  }
+}
+
+interestrequst(){
+  console.log('button clicked');
+  if(this.togglerequest== false){
+     this.togglerequest= true;
+  } else{
+    this.togglerequest= false;
+  }
+}
+
+interestreject(){
+  console.log('button clicked');
+  if(this.togglerejectinterst == false){
+     this.togglerejectinterst = true;
+  } else{
+     this.togglerejectinterst = false;
+  }
+}
+
+newmatch(){
+  console.log('button clicked');
+    if(this.togglenewmatch == false){
+       this.togglenewmatch = true;
+    } else{
+       this.togglenewmatch = false;
+    }
+}
+
+passreset(){
+  console.log('button clicked')
+  if(this.togglepassword== false){
+     this.togglepassword= true;
+  } else{
+     this.togglepassword= false;
+  }
+}
+
+canceladdpageform() {
+  this.togglepassword = true;
+  this.paswordtemplateform.reset();
+}
+
+cancelregisterapproved() {
+  this.toggleregisedit = true;
+  this.registertemplateform.reset();
+}
+
+cancelmemberactivation() {
+  this.toggleactivateedit = true;
+  this.memberactivateform.reset();
+}
+
+cancelpaymentedit() {
+  this.togglepaysuccess = true;
+  this.paymentsuccessform.reset();
+}
+
+cancelinterestrequst() {
+  this.togglerequest = true;
+  this.interestrequestform.reset();
+}
+
+cancelinterestreject() {
+  this.togglerejectinterst = true;
+  this.interestrejectform.reset();
+}
+
+cancelnewmatch() {
+  this.togglenewmatch = true;
+  this.newmatchform.reset();
+}
+
 }
