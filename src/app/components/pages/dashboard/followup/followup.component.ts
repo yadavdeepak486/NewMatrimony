1f7060dcc92520b07deeddf3c34aa93242679483
rohit0035 <rohitdevelopersveltosest@gmail.com>
2021-09-14 10:46:47 +0530

    user-profile

--- src/app/components/pages/dashboard/followup/followup.component.ts
+++ src/app/components/pages/dashboard/followup/followup.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { AdminService } from 'src/app/admin.service';
 
 @Component({
   selector: 'app-followup',
@@ -7,11 +9,16 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./followup.component.scss']
 })
 export class FollowupComponent implements OnInit {
-  users = new FormControl();
-  usersList: string[] = ['Rahul', 'Mohit', 'Mukesh', 'lorem', ];
-  constructor() { }
+  allstaff: any;
+  // users = new FormControl();
+  // usersList: string[] = ['Rahul', 'Mohit', 'Mukesh', 'lorem', ];
+  constructor(
+    public adminService: AdminService,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit(): void {
+    this.getallstaff();
   }
   breadcrumb = [
     {
@@ -19,4 +26,16 @@ export class FollowupComponent implements OnInit {
         subTitle: 'Dashboard'
     }
 ]
+
+getallstaff() {
+  this.adminService.getallstaff().subscribe(
+    (response: any) => {
+      this.allstaff = response.data;
+      console.log(response);
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+}
 }
